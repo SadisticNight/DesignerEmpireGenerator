55a9187391d468d4d64c9d71ca8473c6c44ea097
SadisticNight <jorlumori@gmail.com>
2024-03-29 22:39:11 -0400

    Área 1x1 conseguida!

--- areas.py
+++ areas.py
@@ -13,15 +13,17 @@ class Area:
     @staticmethod
     def area_decoracion(posicion_edificio, radio=4, NUM_CELDAS=200):
         x_centro, y_centro = posicion_edificio
+        posicion_edificio = tuple(posicion_edificio)
         Area.area_afectada.clear()
 
         for x in range(max(0, x_centro - radio), min(NUM_CELDAS, x_centro + radio + 1)):
             for y in range(max(0, y_centro - radio), min(NUM_CELDAS, y_centro + radio + 1)):
-                if (x - x_centro)**2 + (y - y_centro)**2 <= radio**2:
+                if (x - x_centro)**2 + (y - y_centro)**2 <= radio**2 and (x, y) != posicion_edificio:
                     Area.area_afectada.add((x, y))
                     
         print(Area.area_afectada)
         return list(Area.area_afectada)
+
 
     @staticmethod
     def actualizar_celdas(edificio_seleccionado, edificios):
@@ -33,8 +35,8 @@ class Area:
                 celda = next((c for c in celdas_data[_D] if (c['x'], c['y']) == celda_coords), None)
                 if celda is not None:
                     atributos_edificio = edificios[edificio_seleccionado].to_dict()
-                    celda[_V][_E] = atributos_edificio[_E]
-                    celda[_V][_S] = atributos_edificio[_S]
+                    celda[_V][_E] += atributos_edificio[_E]
+                    celda[_V][_S] += atributos_edificio[_S]
             with open(_C, 'wb') as file:
                 pickle.dump(celdas_data, file)
             print(f"Área rellenada")
